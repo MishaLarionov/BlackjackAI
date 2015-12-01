6dd9fa66e07653506f04f94c49e55e01714f82bd
Iain <iaintensity87@gmail.com>
2015-12-01 12:26:06 -0800

    Finished card total method Started decision making process

--- src/ActionSelector.java
+++ src/ActionSelector.java
@@ -4,18 +4,45 @@
  *
  */
 public class ActionSelector
-{	
-	protected ActionSelector()
+{
+	// Reference to the AI
+	private AI ai;
+	protected int total;
+	final int STAND = 1;
+	final int HIT = 0;
+	final int DOUBLE = 2;
+
+	protected ActionSelector(AI ai)
 	{
-		int totalValue = getCardTotal();
-		
+		total = getCardTotal();
+		this.ai = ai;
+		decideMove();
 	}
 	
+	/**
+	 * Decides move based on the total value of the cards
+	 */
+	public int decideMove()
+	{
+		if (total < 8)
+			return HIT;
+		if (total > 16)
+			return STAND;
+		return 0;
+	}
+	
+	
+	/**
+	 * Gets the total of all the cards
+	 * @return
+	 */
 	protected int getCardTotal()
 	{
 		int total = 0;
 
-	//	for(card:)
-		return 0;
+		for (int card = 0; card < ai.myCards.size(); card++)
+			total+= ai.myCards.get(card).intValue();
+
+		return total;
 	}
 }
