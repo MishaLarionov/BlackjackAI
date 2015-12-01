a9ba05cfc694febf30dda3de8b33a3743cb78d46
Iain <iaintensity87@gmail.com>
2015-12-01 12:41:46 -0800

    Added ace checker Started pair checker

--- src/ActionSelector.java
+++ src/ActionSelector.java
@@ -5,44 +5,78 @@
  */
 public class ActionSelector
 {
-	// Reference to the AI
-	private AI ai;
-	protected int total;
+	// Constants for returning decided action
 	final int STAND = 1;
 	final int HIT = 0;
 	final int DOUBLE = 2;
 
+	// AI reference
+	private AI ai;
+
+	// Variables in determining the move
+	protected int total;
+	boolean isAce;
+	boolean isPair;
+
 	protected ActionSelector(AI ai)
 	{
-		total = getCardTotal();
 		this.ai = ai;
-		decideMove();
 	}
-	
+
 	/**
-	 * Decides move based on the total value of the cards
+	 * Decides move based on the total value of the cards, references Felix's
+	 * part when probability is relevant
 	 */
 	public int decideMove()
 	{
+		total = getCardTotal();
+		isAce = isAce();
 		if (total < 8)
 			return HIT;
+
 		if (total > 16)
 			return STAND;
 		return 0;
 	}
-	
-	
+
 	/**
 	 * Gets the total of all the cards
-	 * @return
+	 * @return the total of all cards
 	 */
 	protected int getCardTotal()
 	{
 		int total = 0;
 
 		for (int card = 0; card < ai.myCards.size(); card++)
-			total+= ai.myCards.get(card).intValue();
+			total += ai.myCards.get(card).intValue();
 
 		return total;
+	}
+
+	/**
+	 * Determines if there is an ace in the dealt cards
+	 * @return if there is an ace in the dealt cards
+	 */
+	protected boolean isAce()
+	{
+		for (int card = 0; card < ai.myCards.size(); card++)
+			if (ai.myCards.get(card).intValue() == 1)
+				return true;
+
+		return false;
+	}
+	
+	/**
+	 * Determines if dealt a pair
+	 * @return if dealt a pair
+	 */
+	protected boolean isPair()
+	{
+		for (int card = 0; card < ai.myCards.size();card++)
+		{
+			
+		}
+		
+		return false;
 	}
 }
