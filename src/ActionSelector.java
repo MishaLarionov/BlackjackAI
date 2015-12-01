bfaf20fb065f4dfb434f53a7fcc9175ce1317ff8
Iain <iaintensity87@gmail.com>
2015-12-01 12:09:18 -0800

    Started card total method

--- src/ActionSelector.java
+++ src/ActionSelector.java
@@ -4,14 +4,18 @@
  *
  */
 public class ActionSelector
-{
-	
-	
+{	
 	protected ActionSelector()
 	{
+		int totalValue = getCardTotal();
 		
 	}
+	
+	protected int getCardTotal()
+	{
+		int total = 0;
 
-	
-	
+	//	for(card:)
+		return 0;
+	}
 }
