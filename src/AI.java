292283ca2ddaa4844959a16ead0065e7d2c29cb6
Vince Ou <vince.ou@gmail.com>
2015-12-01 12:28:52 -0500

    Added card tracking

--- src/AI.java
+++ src/AI.java
@@ -7,21 +7,24 @@ import java.util.ArrayList;
 
 public class AI {
 
-	private final short PORT = 100;
 	private Socket server;
 	private PrintWriter serverWrite;
 	private BufferedReader serverRead;
-	
+
 	protected ArrayList<Integer> myCards = new ArrayList<Integer>();
-	protected int[] playedCards = new int[312];
-	
+	// Each index's possible value is 0-24
+	protected int[] playedCards = new int[13];
+	protected short myCoins;
+	protected int dealerFaceUp;
+
 	private ActionSelector decision;
-	
+
 	public static void main(String[] args) {
 		new AI();
 	}
 
 	public AI() {
+		// Asks user for IP of server, etc.
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		System.out.print("What is the server's IP address? ");
@@ -29,26 +32,38 @@ public class AI {
 		try {
 			ip = br.readLine();
 			System.out.println("");
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		while (!ip.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
-			System.out
-					.print("That doesn't look like a valid IPv4 address.\nTry again: ");
-			try {
+
+			while (!ip
+					.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+				System.out
+						.print("That doesn't look like a valid IPv4 address.\nTry again: ");
 				ip = br.readLine();
 				System.out.println("");
-			} catch (IOException e1) {
-				e1.printStackTrace();
 			}
-		}
-
-		try {
-			server = new Socket(ip, PORT);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+		// Gets port of game
+		System.out.print("What port is the server operating on? ");
+		String port = "";
+		try {
+			port = br.readLine();
+			while (!port.matches("[0-9]*")) {
+				System.out
+						.println("That doesn't look like a valid port number.\nTry again: ");
+				port = br.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Sets up connection
+		try {
+			server = new Socket(ip, Integer.parseInt(port));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		try {
 			serverRead = new BufferedReader(new InputStreamReader(
 					server.getInputStream()));
@@ -56,19 +71,38 @@ public class AI {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		decision = new ActionSelector();
 
+		decision = new ActionSelector(this);
+
+		// Init connection w/ server
+		serverWrite.println("player");
+		serverWrite.flush();
 		serverWrite.print("VinceFelixIainAI");
 		serverWrite.flush();
-		
+
+		// Gets info about current player
 		String introInfo = "";
 		try {
 			introInfo = serverRead.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		int myPlayerNumber = Integer.parseInt(introInfo.substring(0, introInfo.indexOf(' ')));
+		int myPlayerNumber = Integer.parseInt(introInfo.substring(0,
+				introInfo.indexOf(' ')));
+
+		// Waits for game to start
+		// They broadcast a bunch of useless stuff like
+		// "this other player joined, etc."
+		try {
+			while (!serverRead.readLine().equals("START"))
+				System.out.println("waiting for game to start");
+			// For the  first "NEWROUND" message
+			serverRead.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		myCoins = 1000;
 		
 	}
 
