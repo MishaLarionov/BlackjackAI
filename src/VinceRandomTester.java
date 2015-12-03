61d26cdd1692c4e6d1af727941d848006b4e246b
Vince Ou <vince.ou@gmail.com>
2015-12-03 19:30:31 +0000

    Started on the random tester

---
+++ src/VinceRandomTester.java
@@ -0,0 +1,13 @@
+class VinceRandomTester {
+
+    final static char STAND = 's';
+    final static char HIT = 'h';
+
+    VinceRandomTester() {
+    
+    }
+    
+    char pickAction(int) {
+        
+    }
+}
