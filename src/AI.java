bb7dca6afb9697880c1676beddf4a6a2548b36ae
Vince Ou <vince.ou@gmail.com>
2015-12-07 12:49:53 -0500

    ==UNFINISHED== Redoing IO from server.

--- src/AI.java
+++ src/AI.java
@@ -14,9 +14,13 @@ public class AI {
 	protected short myCoins;
 
 	private ActionSelector decision;
-	private int BET_AMOUNT = 1;
+	// TODO make something to dynamically change the bet amount
+	private int betAmount = 1;
 	private int myPlayerNumber;
-	private String USER_NAME = "VinceFe%20lix\n\\#$ !*%@IainAI";
+	private String USER_NAME = "VinceIainFelix";
+	private String loopEndStr = "";
+
+	private final static boolean DEBUG = true;
 
 	public static void main(String[] args) {
 		try {
@@ -34,6 +38,8 @@ public class AI {
 		String ip = "";
 
 		ip = br.readLine();
+		if (DEBUG)
+			ip = "10.242.166.206";
 		System.out.println("");
 
 		while (!ip.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
@@ -66,40 +72,41 @@ public class AI {
 
 		decision = new ActionSelector();
 
-		// Init connection w/ server
-		serverWrite.println("PLAY");
-		serverWrite.flush();
+		// Send name to server
+		sendMessage(USER_NAME);
+
+		// Tell server we want to be a player
+		sendMessage("PLAY");
 
 		// So apparently you can spam the server with requests to play every
 		// second if you're not accepted right away. So that's what we're gonig
 		// to do.
-		while (!serverRead.readLine().equals("% ACCEPTED")) {
-			serverWrite.println("PLAY");
-			serverWrite.flush();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		// When finally accepted into the lobby, gives them our name.
-		serverWrite.print(USER_NAME);
-		serverWrite.flush();
+		/*
+		 * // So apparently the following is just a feature request. Delete when
+		 * done. while (!serverRead.readLine().equals("% ACCEPTED")) {
+		 * serverWrite.println("PLAY"); serverWrite.flush(); try {
+		 * Thread.sleep(1000); } catch (InterruptedException e) {
+		 * e.printStackTrace(); } }
+		 */
 
 		// Gets info about current player from server
-		myPlayerNumber = Integer.parseInt(serverRead.readLine().split(" ")[1]
-				.trim());
+		if (!getMessage().equals("% ACCEPTED")) {
+			System.out.println("Failed to accept request to server.");
+			try {
+				server.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.exit(0);
+		}
+
+		myPlayerNumber = Integer.parseInt(getMessage("@").split(" ")[1].trim());
 
 		// Waits for game to start
 		// They broadcast a bunch of useless stuff like
 		// "this other player joined, etc."
-		serverWrite.println("READY");
-		serverWrite.flush();
-		while (!serverRead.readLine().startsWith("% ST"))
-			System.out
-					.println("waiting for game to start, other players are joining");
-		System.out.println("Game has started.");
+		sendMessage("READY");
+		getMessage("% START");
 		myCoins = 1000;
 
 		// TODO add support for multiple rounds
@@ -108,30 +115,25 @@ public class AI {
 		}
 	}
 
-	private void runRound() throws IOException {
+	private void runRound() {
 		// Waits for the server to start a new round
-		while (!serverRead.readLine().equals("% NEWROUND"))
-			System.out.println("Waiting for new round to start");
+		getMessage("% NEWROUND");
 		System.out.println("Round has started");
+		decision.resetHand();
 
 		// Tell server bet amount, and
-		serverWrite.println(BET_AMOUNT);
+		serverWrite.println(betAmount);
 		serverWrite.flush();
 
 		// Server will broadcast bets by other players, so ignore that.
-		String bet = serverRead.readLine();
-		while (bet.startsWith("$")) {
-			System.out.println("Another bet placed by a player,"
-					+ "still waiting for dealing to begin.");
-			// The last "bet" that we read is actually a card deal, so we need
-			// to accomodate for that.
-			bet = serverRead.readLine();
-		}
 
 		// Takes all the cards that the server deals to all players
-		String dealString = bet;
-		while (dealString.charAt(0) == '#') {
+		ArrayList<String> bets = getAllMessages("#");
+//		String dealString = bet;
+		// while (dealString.charAt(0) == '#') {
+		for (int i = 0; i < bets.size(); i++) {
 			// Gets the actual information
+			String dealString = bets.get(i);
 			String[] cardDeal = dealString.split(" ");
 			int playerNum = Integer.parseInt(cardDeal[1]);
 			char cardChar = cardDeal[2].charAt(0);
@@ -149,39 +151,37 @@ public class AI {
 				}
 
 				// Adds it to the played cards for counting purposes
-				decision.addToMyHand(new Card(cardNum));
+				decision.cardPlayed(new Card(cardNum));
 			}
-
-			// Gets the next deal from server
-			while (!serverRead.ready())
-				System.out
-						.println("Waiting for server to send out another deal");
-			dealString = serverRead.readLine();
 		}
 
-		// Waits for my turn
-		String turnStr = dealString;
-		while (!turnStr.equals("% " + myPlayerNumber + " turn")) {
-			System.out.println("Waiting for turn");
-			turnStr = serverRead.readLine();
-		}
-
-		runMyTurn();
+//		// Waits for my turn
+//		String turnStr = dealString;
+//		while (!turnStr.equals("% " + myPlayerNumber + " turn")) {
+//			System.out.println("Waiting for turn");
+//			turnStr = serverRead.readLine();
+//		}
+//
+//		runMyTurn();
+//
+//		while (turnStr.startsWith("%")) {
+//			turnStr = serverRead.readLine();
+//		}
+		
+		getMessage("% " + myPlayerNumber + " turn");
+		
 
 		while (!turnStr.startsWith("# 0")) {
 			turnStr = serverRead.readLine();
 			System.out.println("Waiting for the server to reveal their cards");
 		}
-		// TODO add padding between my move and the server's card revealing
 
 		// Gets the face-down card and other cards that the dealer pulls
 		String remainingCards = serverRead.readLine();
 		while (remainingCards.startsWith("#")) {
 			String[] rCards = remainingCards.split(" ");
 			decision.cardPlayed(new Card(Integer.parseInt(rCards[2])));
-
-			if (serverRead.ready())
-				remainingCards = serverRead.readLine();
+			remainingCards = serverRead.readLine();
 		}
 
 		String updateStr = turnStr;
@@ -198,6 +198,11 @@ public class AI {
 				}
 			}
 		}
+
+		if (updateStr.equals("% SHUFFLE")) {
+			decision.resetCardCounter();
+		}
+		loopEndStr = updateStr;
 	}
 
 	private int parseCard(char cardChar) {
@@ -248,5 +253,66 @@ public class AI {
 			System.out.println("Sent stand to server");
 		}
 		serverWrite.flush();
+	}
+
+	private String getMessage(String regex) {
+		String message = "";
+		while (!message.startsWith(regex)) {
+			try {
+				message = serverRead.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (DEBUG) {
+				System.out.println("Response from server: " + message);
+			}
+		}
+		return message;
+	}
+
+	private String getMessage() {
+		String message = "";
+		try {
+			message = serverRead.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (DEBUG) {
+			System.out.println("Response from server: " + message);
+		}
+		return message;
+	}
+
+	private ArrayList<String> getAllMessages(String regex) {
+		ArrayList<String> messages = new ArrayList<String>();
+		String message = "";
+		while (!message.startsWith(regex)) {
+			try {
+				message = serverRead.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		do {
+			try {
+				messages.add(serverRead.readLine());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} while (message.startsWith(regex));
+		return messages;
+	}
+
+	// TODO check if double down is possible based on amount of coins
+
+	private void sendMessage(String message) {
+		try {
+			serverWrite.println(message);
+			serverWrite.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (DEBUG)
+			System.out.println("Sent message to server: " + message);
 	}
 }
