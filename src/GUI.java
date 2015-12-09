eeb163af726952227bd0211e337a84b4a8098fa8
Vince Ou <vince.ou@gmail.com>
2015-12-09 08:43:34 -0500

    Started on GUI.

--- src/GUI.java
+++ src/GUI.java
@@ -1,22 +1,47 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GUI {
+public class GUI extends JFrame {
+
+	private AI ai;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new GUI();
 	}
 
-	class MonitorFrame extends JFrame {
-		public MonitorFrame() {
-			// TODO Auto-generated constructor stub
-		}
+	public GUI() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setPreferredSize(new Dimension(300, 200));
+		this.setResizable(false);
+		this.add(new MonitorPanel());
+		this.pack();
+		this.setVisible(true);
 	}
 
 	class MonitorPanel extends JPanel {
 		public MonitorPanel() {
-			// TODO Auto-generated constructor stub
+			this.setBackground(Color.WHITE);
+
+			String ip = JOptionPane.showInputDialog(
+					"Please enter the IP of the server", "127.0.0.1");
+			while (!ip
+					.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+				ip = JOptionPane.showInputDialog(
+						"That doesn't look like a proper IPv4 address",
+						"127.0.0.1");
+			}
+
+			String port = JOptionPane.showInputDialog(
+					"Please enter the port of the server", "1234");
+			while (!port.matches("[0-9]*"))
+				port = JOptionPane.showInputDialog(
+						"That doesn't look like a valid port", "1234");
+
+			ai = new AI(ip, Integer.parseInt(port));
 		}
 	}
 
