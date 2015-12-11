992c347d2ee6454515477ef3f19b49d637fb20cb
Vince Ou <vince.ou@gmail.com>
2015-12-11 12:50:22 -0500

    Created a second decision maker.

--- src/decisions/ActionSelector2.java
+++ src/decisions/ActionSelector2.java
@@ -1,5 +1,69 @@
 package decisions;
 
+import java.util.ArrayList;
+
+import objects.Card;
+import objects.Hand;
+
 public class ActionSelector2 {
 
+	private Hand myHand;
+	private int THRESHOLD = 17;
+
+	public ActionSelector2() {
+		return;
+	}
+
+	protected void resetCardCounter() {
+		return;
+	}
+
+	protected int decideMove(boolean doubleDownAllowed) {
+		ArrayList<Integer> totals = removeIrrelevant(myHand.recalcTotals());
+		double average = averageOfTotal(totals);
+		if (totals.contains(21))
+			return ActionSelector.STAND;
+
+		if (average < THRESHOLD)
+			return ActionSelector.HIT;
+		else
+			return ActionSelector.STAND;
+	}
+
+	protected void resetHand() {
+		myHand = new Hand();
+	}
+
+	protected void addToMyHand(Card newCard) {
+		myHand.add(newCard);
+	}
+
+	protected Hand getMyHand() {
+		return myHand;
+	}
+
+	protected void setDealerCard(Card newCard) {
+		return;
+	}
+
+	protected void cardPlayed(Card newCard) {
+		return;
+	}
+
+	private ArrayList<Integer> removeIrrelevant(ArrayList<Integer> totals) {
+		for (int i = 0; i < totals.size(); i++) {
+			if (totals.get(i) >= 21)
+				totals.remove(i);
+		}
+		return totals;
+	}
+
+	private double averageOfTotal(ArrayList<Integer> totals) {
+		double total = 0;
+		for (Integer integer : totals) {
+			total += integer;
+		}
+		total /= totals.size();
+		return total;
+	}
 }
